323cc867b6c0e724eae2ffbee3a3e4b7efde04b2
viktak <github@viktak.com>
2020-12-29 18:13:19 +0200

    lib_deps brought up to date

--- include/version.h
+++ include/version.h
@@ -7,11 +7,11 @@
 ///////////////////////////////////////////////////////////////
 
 #ifndef BUILD_NUMBER
-  #define BUILD_NUMBER "2"
+  #define BUILD_NUMBER "10"
 #endif
 #ifndef FIRMWARE_VERSION
-  #define FIRMWARE_VERSION "v1.1.2 - 2020-12-29 17:19:13.423647"
+  #define FIRMWARE_VERSION "v1.1.10 - 2020-12-29 18:08:24.976737"
 #endif
 #ifndef FIRMWARE_VERSION_SHORT
-  #define FIRMWARE_VERSION_SHORT "v1.1.2"
+  #define FIRMWARE_VERSION_SHORT "v1.1.10"
 #endif
